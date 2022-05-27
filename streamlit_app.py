232ca16ed75bb12766a6c3bdf0b94f8fb70783ed
robot-monk <106399432+robot-monk@users.noreply.github.com>
2022-05-27 13:15:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,25 @@
+@@ -1,21 +1,44 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
 
+"""
+# Welcome to Streamlit!
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 6000, 2000)
     total_points = st.text("Amount of income", 1, 6000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
     Point = namedtuple('Point', 'x y')
     data = []
 
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
     points_per_turn = total_points / num_turns
 
     for curr_point_num in range(total_points):
@@ -19,8 +32,10 @@ with st.echo(code_location='below'):
 
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q')
+        .encode(x='x:Q', y='y:Q'))
+0 comments on commit d8af0ae
 with st.echo(code_location='below'):
+    total_points = st.slider("Amount of income", 1, 6000, 0)
     total_points = st.slider("Number of points in spiral", 1, 6000, 2000)
     total_points = st.text("Amount of income", 1, 6000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
