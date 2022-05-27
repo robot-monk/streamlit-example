6a0403d4e3a05a4fa889211f93244ae7ca046f86
robot-monk <106399432+robot-monk@users.noreply.github.com>
2022-05-27 13:30:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,27 @@
+@@ -1,4 +1,3 @@
+
 from collections import namedtuple
 import altair as alt
 import math
-import pandas as pd
-import streamlit as st
+@@ -7,37 +6,18 @@
 
 """
 # Welcome to Streamlit!
-
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
+with st.echo(code_location='below'):
+    total_points = st.slider("Number of points in spiral", 1, 6000, 2000)
+    total_points = st.text("Amount of income", 1, 6000, 2000)
+    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Income", 1, 100000000000000000000000000000000000000000, 0)
-    num_turns = st.slider("Materials Price", 1, 100000000000000000000000000000000000, 0)
-
     Point = namedtuple('Point', 'x y')
     data = []
-
-    points_per_turn = total_points - num_turns
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
+    points_per_turn = total_points / num_turns
 
     for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, money)
+        curr_turn, i = divmod(curr_point_num, points_per_turn)
         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
         radius = curr_point_num / total_points
         x = radius * math.cos(angle)
@@ -36,9 +31,11 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
-    
-    
-    
-    st.write("""My first Streamlit""")
-    
-  
+
+with st.echo(code_location='below'):
+    total_points = st.slider("Amount of income", 1, 6000, 0)
+    total_points = st.slider("Number of points in spiral", 1, 6000, 2000)
+    total_points = st.text("Amount of income", 1, 6000, 2000)
+    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+
+    Point = namedtuple('Point', 'x y')
